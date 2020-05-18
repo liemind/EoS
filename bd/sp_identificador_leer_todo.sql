7140c4149f01d1a37b58882d0cda7e64dcdb839d
Daniela Paredes <elynawatcher@gmail.com>
2020-05-18 12:42:39 -0400

    Updated
    Delete files for security

--- bd/sp_identificador_leer_todo.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-DROP PROCEDURE IF EXISTS sp_identificador_leer_todo $$
-CREATE PROCEDURE sp_identificador_leer_todo()
-BEGIN
-	SELECT id,compuesto,formula, m
-	FROM identificador;
-END
-$$
